bbc7f833356ac05adb3e50ca905771efafa03cf4
Tobias Östh <tobias.osth@coor.com>
2024-03-10 13:29:35 +0100

    pattern matching begun for extraction of images and links from MD

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,7 +1,8 @@
-from textnode import (
-    TextNode,
-    text_type_text,
-)
+import re
+
+from textnode import TextNode, text_type_text
+
+pattern = re.pattern(r"!\[(.*?)\]\((.*?)\)")
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -11,10 +12,20 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             continue
         split_by_delimiter = node.text.split(delimiter)
         if len(split_by_delimiter) % 2 == 0:
-            raise Exception(f"This isn't valid markdown: {node.text}. \nMissing opening or closing tag for {text_type} with delimiter {delimiter}")
+            raise Exception(
+                f"This isn't valid markdown: {node.text}. \nMissing opening or closing tag for {text_type} with delimiter {delimiter}"
+            )
         for i in range(0, len(split_by_delimiter)):
+            if split_by_delimiter[i] == "":
+                continue
             if i == 0 or i % 2 == 0:
                 new_nodes.append(TextNode(split_by_delimiter[i], text_type_text))
             else:
                 new_nodes.append(TextNode(split_by_delimiter[i], text_type))
     return new_nodes
+
+def extract_markdown_images(text):
+    
+
+
+def extract_markdown_links(text):
