363215d9919c071e6f20f7d3cae30657bc4f3ac5
Tobias Östh <tobias.osth@coor.com>
2024-03-11 14:35:22 +0100

    Extracting images and links are now working correctly according to tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -33,32 +33,51 @@ def extract_markdown_links(text):
 
 
 def split_nodes_image(old_nodes):
-    print(old_nodes)
     new_nodes = []
     for node in old_nodes:
         if node.text_type != text_type_text:
             new_nodes.append(node)
             continue
-        split_by_delimiter = node.text.split(r"!\[(.*?)\]\((.*?)\)")
-        if len(split_by_delimiter) % 2 == 0:
-            raise Exception(
-                f"This isn't valid markdown: {node.text}. \nMissing image tag in markdown text"
-            )
-        for i in range(0, len(split_by_delimiter)):
-            if split_by_delimiter[i] == "":
+        words = re.split(r"!\[(.*?)\]\((.*?)\)", node.text)
+        images = extract_markdown_images(node.text)
+        for word in words:
+            no_match = True
+            if word == "":
                 continue
-            if i == 0 or i % 2 == 0:
-                images = extract_markdown_images(split_by_delimiter[i])
-                if len(images) == 0:
-                    continue
-                for img in images:
-                    new_nodes.append(TextNode(img[0], text_type_image, img[1]))
-            else:
-                new_nodes.append(TextNode(split_by_delimiter[i], text_type_text))
-    print(new_nodes)
+            for image in images:
+                if image[1] == word:
+                    no_match = False
+                    break
+                if image[0] == word:
+                    new_nodes.append(TextNode(image[0], text_type_image, image[1]))
+                    no_match = False
+                    break
+            if no_match == True:
+                new_nodes.append(TextNode(word, text_type_text, None))
+
     return new_nodes
 
 
 def split_nodes_link(old_nodes):
     new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        words = re.split(r"\[(.*?)\]\((.*?)\)", node.text)
+        links = extract_markdown_links(node.text)
+        for word in words:
+            no_match = True
+            if word == "":
+                continue
+            for link in links:
+                if link[1] == word:
+                    no_match = False
+                    break
+                if link[0] == word:
+                    new_nodes.append(TextNode(link[0], text_type_link, link[1]))
+                    no_match = False
+                    break
+            if no_match == True:
+                new_nodes.append(TextNode(word, text_type_text, None))
     return new_nodes
